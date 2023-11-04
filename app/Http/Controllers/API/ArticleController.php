fa59c141b8b0070455ed3d89e819bb86adebd2b9
yaman <ykassem957@gmail.com>
2023-11-04 08:15:50 +0200

    fet(article and tags flow)

---
+++ app/Http/Controllers/API/ArticleController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Models\Article;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\ArticleResource;
+use App\Http\Requests\StoreArticleRequest;
+use App\Http\Requests\UpdateArticleRequest;
+
+class ArticleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $articles = Article::where('deleted_at', NULL)->get();
+        return response()->json(ArticleResource::collection($articles), 200);
+    }
+
+    /**
+     * Display a listing of the deleted resource.
+     */
+    public function deleted_articles()
+    {
+        $articles = Article::onlyTrashed()->get();
+        return response()->json(ArticleResource::collection($articles), 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreArticleRequest  $request)
+    {
+        $validated = $request->validated();
+
+        $article = new Article();
+
+        $article->title = $request->title;
+        $article->summary = $request->summary;
+        $article->description = $request->description;
+        $article->lang = $request->lang;
+
+        $article->save();
+
+        return response()->json(new ArticleResource($article), 200);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $article = Article::findOrFail($id);
+        return response()->json(new ArticleResource($article), 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateArticleRequest $request, Article $article)
+    {
+        $validated = $request->validated();
+
+        $article->update([
+            'title' => $request->title ?? $article->title,
+            'summary' => $request->summary ?? $article->summary,
+            'description' => $request->description ?? $article->description,
+            'lang' => $request->lang ?? $article->lang,
+        ]);
+
+        return response()->json(new ArticleResource($article), 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Article $article)
+    {
+        $article->delete();
+        return response()->json('Deleted Done', 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\API\ArticleController;
+use App\Http\Controllers\API\TagController;
+use App\Models\Article;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +20,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::apiResource('articles',ArticleController::class);
+
+Route::get('/deleted',[ArticleController::class,'deleted_articles']);
+
+Route::apiResource('tags',TagController::class);
